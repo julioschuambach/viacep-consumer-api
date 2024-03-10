6c22940bab30debbe1556080176ba2b1b8cff631
julioschuambach <julioschuambach.dev@gmail.com>
2024-03-10 16:44:55 -0300

    feat: created database context

---
+++ ViaCepConsumer/ViaCepConsumer.Api/Infrastructure/Contexts/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ViaCepConsumer.Api.Entities;
+
+namespace ViaCepConsumer.Api.Infrastructure.Contexts
+{
+    public class DatabaseContext : DbContext
+    {
+        private string _connectionString = "Server = localhost, 1433; Database = ViaCepConsumer; User ID = sa; Password = 1q2w3e4r@#$;";
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer(_connectionString);
+    }
+}
